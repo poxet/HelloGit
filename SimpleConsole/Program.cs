97e6563783d3f1c7824b37a6820ee3189b2fdc91
Daniel Bohlin <daniel.bohlin@gmail.com>
2015-10-07 00:10:57 +0200

    Added simple console application

---
+++ SimpleConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SimpleConsole
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: Write code here!
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
